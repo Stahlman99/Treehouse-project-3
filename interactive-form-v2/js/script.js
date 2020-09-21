1aa03788d78d1b784c393523da5098c94be0511d
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-09-21 10:45:06 -0500

    Added an input for the 'other' job role and made the color options available only with the appropriate designs.

--- interactive-form-v2/js/script.js
+++ interactive-form-v2/js/script.js
@@ -1,18 +1,89 @@
+// Variable Declarations
+
 
 // Created variables that hold the input fields.
 const nameInput = document.getElementById('name');
 const jobRoleInput = document.getElementById('title');
 const otherInput = document.getElementById('other-title');
+const colorSelect = document.getElementById('color');
+const colorSelectDefault = document.createElement('option');
+colorSelectDefault.value = "default";
+colorSelectDefault.text = 'Please select a T-shirt theme';
+colorSelectDefault.style.display = "block";
+const designSelect = document.getElementById('design');
+
+
+// Call functions and set up page.
+
 
 // Sets the focus on the nameInput
 nameInput.focus();
-// Hide the other title input field
-otherInput.type = 'hidden';
+
+// Hide select and input fields
+hideItem(otherInput);
+
+// Set colorSelect's default value
+colorSelect.insertAdjacentElement("afterbegin",colorSelectDefault);
+colorSelect.value = "default";
+
+// Set Values
+for (let i = 0; i < colorSelect.length; i++) {
+    hideItem(colorSelect[i]);
+}
+
+
+// Functions
+
+
+// Creates function to display elements
+function displayItem(itemToDisplay) {
+    itemToDisplay.style.display = 'block';
+}
+
+// Creates function to hide elements
+function hideItem(itemToDisplay) {
+    itemToDisplay.style.display = 'none';
+}
+
+
+// Event Listeners
+
 
 // Create event listener for the jobRoleInput and reveal the otherInput field when 'other' is selected.
 jobRoleInput.addEventListener('click', (e) => {
     if (e.target.value === 'other') {
-        console.log('It works!');
-        otherInput.type = 'text';
+        displayItem(otherInput);
+    }
+});
+
+designSelect.addEventListener('click', (e) => {
+    if (e.target.value === 'js puns') {
+
+        for (let i = 0; i < colorSelect.children.length; i++) {
+            let val = colorSelect[i].value;
+            if (val === 'tomato' || val === 'steelblue' || val === 'dimgrey' || val === 'default') {
+                hideItem(colorSelect[i]);
+            } else {
+                displayItem(colorSelect[i]);
+            }
+
+            colorSelect.value = 'default';
+        }
+    } else if (e.target.value === 'heart js') {
+        for (let i = 0; i < colorSelect.children.length; i++) {
+            let val = colorSelect[i].value;
+            if (val === 'tomato' || val === 'steelblue' || val === 'dimgrey') {
+                displayItem(colorSelect[i]);
+            } else {
+                hideItem(colorSelect[i]);
+            }
+
+            colorSelect.value = 'default';
+        }
+    } else {
+        colorSelect.value = 'default';
+        for (let i = 0; i < colorSelect.children.length; i++) {
+            hideItem(colorSelect[i]);
+        }
     }
 });
